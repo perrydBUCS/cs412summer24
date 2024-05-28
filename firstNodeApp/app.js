d582579af8aabccea983cdff670d1cfa0feeba7a
Perry D <35308576+perrydBUCS@users.noreply.github.com>
2024-05-28 14:55:35 -0400

    dding wx api call, just returning text

--- firstNodeApp/app.js
+++ firstNodeApp/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const bazeebleRouter = require('./routes/wx');
+const wxRouter = require('./routes/wx');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter); //we say that we are "mounting" the file on this URL...
-app.use('/bazeeble', bazeebleRouter);
+app.use('/wx', wxRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- firstNodeApp/routes/wx.js
+++ firstNodeApp/routes/wx.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/heyb',
-    (req,
-     res,
-     next) => {
-        res.send('now');
-    });
+//openweather returns times as UNIX timestamps
+
+const requestOptions = {
+    method: "GET",
+    redirect: "follow"
+};
+//match http://localhost:3000/wx/current...
+//
+router.get('/current', (req, res, next) => {
+
+fetch("https://api.openweathermap.org/data/3.0/onecall?lat=42.3&lon=-71.1&exclude=minutely,hourly,daily,alerts&appid=6d13b989d2175d527ad89638956aec97&units=imperial", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+next();
+})
+
 
 module.exports = router;
