0b31871a934b596da8d7341b1387c39fdaa08729
Perry D <35308576+perrydBUCS@users.noreply.github.com>
2024-05-29 15:03:15 -0400

    passing data in routes

--- =passingDataAndEJS/routes/index.js
+++ =passingDataAndEJS/routes/index.js
@@ -1,9 +1,43 @@
+//Passing data into routes
+//
+//REST (Representational state transfer)
+//
+//CRUD = data operation (create, read, update, delete)
+//
+//POST: Create
+//GET: Read
+//PUT: Update
+//DELETE: Delete
+//
 var express = require('express');
 var router = express.Router();
 
+//Way 1: Query strings, params are on req.query
+//
+router.get('/way1', (req, res, next) => {
+console.log(`Got these values on a ${req.method}`);
+let upName = req.query.name.toString().toUpperCase();
+res.send(`Got these values: ${req.query.name} plays ${req.query.instrument}. Really, ${upName}!`);
+})
+//Way 2: Use a POST, values are on req.body
+//
+router.post('/way1', (req, res, next) => {
+  console.log(`Got these values on a ${req.method}`);
+  res.send(`Got these values: ${req.body.name} plays ${req.body.instrument}`);
+})
+
+//Way 3: Named params, on req.params, e.g. http://localhost:3000/way1/Phil/bass
+//
+router.get('/way1/:name/:instrument', (req, res, next) => {
+  console.log(`Got these values on a ${req.method}`);
+
+
+})
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
