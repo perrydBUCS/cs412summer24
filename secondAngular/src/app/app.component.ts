4ffcd837e8021c434d850117df1a920c0971e3a5
Perry D <35308576+perrydBUCS@users.noreply.github.com>
2024-06-11 14:37:19 -0400

    adding weather component to separate HTML file

--- secondAngular/src/app/app.component.ts
+++ secondAngular/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {WeatherComponent} from "./weather/weather.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, WeatherComponent],
+//  templateUrl: '',./app.component.html
+  template: '<h1>Hey now!</h1> <p>Welcome, {{title}} </p>' +
+    '<br/>Your value is: {{theValue}}' +
+    '<br/>' +
+    '<button (click)="changeMe()">Increment</button>' +
+  '<button (click)="lowerMe()">Decrement</button>' +
+    '<hr/>' +
+    '<app-weather></app-weather>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'secondAngular';
+  title = 'firstAngular';
+  theValue: number = 1;
+
+  changeMe = () => {
+    this.theValue++;
+  }
+  lowerMe = ()  => {
+    this.theValue--;
+  }
 }
